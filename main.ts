d3363fecac46e56ec68c32dd2533ed01694169eb
yunhyeyul <yunhyeyul@goedu.kr>
2026-03-27 09:10:34 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+	
+})
+basic.forever(function () {
+	
+})
